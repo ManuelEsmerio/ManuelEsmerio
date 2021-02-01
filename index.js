2a4b47e30ca2069fa1239fc0f867651318617633
Manuel Esmerio Garcia <manuel.esmerio1995@gmail.com>
2021-02-01 12:20:31 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const handleClick = () =>{
+    console.log("Click button suscribete (Suscribete)");
+}
+
+const handleClickLike = () =>{
+    console.log("Click button (Dale like)");
+}
+
+const handlePlus = (n1 = 10, n2 = 5) =>{
+    console.log(`Se sumaran los numero ${n1} + ${n2} dando como resultado: ${n1 + n2}`);
+    return n1 + n2;
+}
